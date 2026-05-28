6bee4d9484de9b069b6816d7676f1888bd0d202c
vanshika <gohelvanshika565@gmail.com>
2026-05-28 08:51:53 +0530

    reactangle

---
+++ my js/AreaOfRecantagl tsc/areaOfRectangle.js
@@ -0,0 +1,5 @@
+"use strict";
+let Length = 5.5;
+let width = 6.5;
+let area = Length * width;
+console.log('Area Of Rectangle: ', area);
---
+++ my js/AreaOfRecantagl tsc/areaOfRectangle.ts
@@ -0,0 +1,6 @@
+let Length : number = 5.5;
+let width : number = 6.5;
+
+let area : number = Length * width;
+
+console.log('Area Of Rectangle: ',area);
