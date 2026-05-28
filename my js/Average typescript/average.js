4e1621cef528765e60a9e08537e5fb9ff38e66cf
vanshika <gohelvanshika565@gmail.com>
2026-05-28 08:09:32 +0530

    average

---
+++ my js/Average typescript/average.js
@@ -0,0 +1,7 @@
+"use strict";
+let marks1 = 44;
+let marks2 = 83;
+let marks3 = 98;
+let total = marks1 + marks2 + marks3;
+let average = total / 3;
+console.log('Average is : ', average);
---
+++ my js/Average typescript/average.ts
@@ -0,0 +1,7 @@
+let marks1:number = 44;
+let marks2:number = 83;
+let marks3:number = 98;
+
+let total:number = marks1 + marks2 + marks3;
+let average : number = total/3;
+console.log('Average is : ',average)
