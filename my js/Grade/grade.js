4e86b629014ba4f2fc12347dcefde5d3310a48fd
vanshika <gohelvanshika565@gmail.com>
2026-05-28 17:30:51 +0530

    grade

---
+++ my js/Grade/grade.js
@@ -0,0 +1,14 @@
+"use strict";
+let marks = 87;
+if (marks >= 90) {
+    console.log("Grade A");
+}
+else if (marks >= 75) {
+    console.log("Grede B");
+}
+else if (marks >= 50) {
+    console.log("Grede C");
+}
+else {
+    console.log("Fail");
+}
---
+++ my js/Grade/grade.ts
@@ -0,0 +1,14 @@
+let marks: number = 87;
+
+if (marks >= 90){
+    console.log("Grade A");
+}
+else if(marks>=75){
+    console.log("Grede B");
+}
+else if(marks>=50){
+    console.log("Grede C");
+}
+else{
+    console.log("Fail");
+}
