96645c590b51f787ae52e8053d4e48173859d96b
vanshika <gohelvanshika565@gmail.com>
2026-05-30 15:49:06 +0530

    bg color,image

---
+++ my js/color-change/script.js
@@ -0,0 +1,18 @@
+function changeBackground(mybg){
+    if(mybg === 'color'){
+        document.body.style.backgroundColor = "pink";
+        document.body.style.backgroundColor = "none"
+    }
+    else if(mybg === 'gradiant'){
+        document.body.style = "background:linear-gradient(rgba(254, 40, 40, 0.785),rgba(71, 222, 222, 0.895))";
+    }
+    else if(mybg === 'image'){
+        document.body.style = "https://i.pinimg.com/736x/6a/80/94/6a809480bd503ce1679de1598d37b306.jpg";
+
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundPosition = "center";
+    }
+    else{
+        alert("Invalid Type.!!");
+    }
+}
