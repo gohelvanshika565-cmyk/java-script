0a20fa34e212808d933b794949b3b7096e2fdebd
vanshika <gohelvanshika565@gmail.com>
2026-05-30 15:55:16 +0530

    image

--- my js/color-change/script.js
+++ my js/color-change/script.js
@@ -7,7 +7,7 @@ function changeBackground(mybg){
         document.body.style = "background:linear-gradient(rgba(254, 40, 40, 0.785),rgba(71, 222, 222, 0.895))";
     }
     else if(mybg === 'image'){
-        document.body.style = "https://i.pinimg.com/736x/6a/80/94/6a809480bd503ce1679de1598d37b306.jpg";
+        document.body.style = "background-image: url('https://i.pinimg.com/736x/6a/80/94/6a809480bd503ce1679de1598d37b306.jpg')";
 
         document.body.style.backgroundSize = "cover";
         document.body.style.backgroundPosition = "center";
